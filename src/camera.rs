49cf9334c278bad45fb5e3241e3e4f556368f0a8
Elhanan Buchsbaum <elhananby@gmail.com>
2024-07-05 16:36:36 +0200

    Refactor camera.rs to set AEAG and simplify code

--- src/camera.rs
+++ src/camera.rs
@@ -1,21 +1,16 @@
-
 use super::cli::Args;
-
 
 /// Dealing with camera parameters
 pub fn set_camera_parameters(cam: &mut xiapi::Camera, args: &Args) -> Result<(), i32> {
-    // lens mode
-    //set_lens_mode_with_retry(cam, args.aperture)?;
-
     // resolution
     set_resolution(cam, args.width, args.height, args.offset_x, args.offset_y)?;
 
     // exposure
-    let adjusted_exposure = adjust_exposure(args.exposure, &args.fps);
-    cam.set_exposure(args.exposure)?;
+    // let adjusted_exposure = adjust_exposure(args.exposure, &args.fps);
+    // cam.set_exposure(args.exposure)?;
 
-    log::info!("Exposure set to: {}", adjusted_exposure);
-    log::info!("FPS set to: {}", args.fps);
+    //log::info!("Exposure set to: {}", adjusted_exposure);
+    //log::info!("FPS set to: {}", args.fps);
 
     // data format
     cam.set_image_data_format(xiapi::XI_IMG_FORMAT::XI_MONO8)?;
@@ -29,35 +24,21 @@ pub fn set_camera_parameters(cam: &mut xiapi::Camera, args: &Args) -> Result<(),
     cam.set_acq_buffer_size(buffer_size * 4)?;
     cam.set_buffers_queue_size(cam.buffers_queue_size_maximum()?)?;
 
-    // Flat-field correction
-    // TODO: Implement flat-field correction
-    // unsafe {
-    //     let image = String::from("/home/buchsbaum/Documents/dark_image.tif");
-    //     let c_path = CString::new(image.clone()).expect("Failed to convert to CString");
-    //     let raw_ptr = c_path.as_ptr();
-    //     let c_void_ptr: *mut c_void = raw_ptr as *mut c_void;
-
-    //     xiapi::xiSetParamString(
-    //         **cam,
-    //         xiapi::XI_PRM_FFC_DARK_FIELD_FILE_NAME.as_ptr() as *const i8,
-    //         c_void_ptr,
-    //         image.len() as u32,
-    //     );
-
-    //     let image = String::from("/home/buchsbaum/Documents/mid_image.tif");
-    //     let c_path = CString::new(image.clone()).expect("Failed to convert to CString");
-    //     let raw_ptr = c_path.as_ptr();
-    //     let c_void_ptr: *mut c_void = raw_ptr as *mut c_void;
-
-    //     xiapi::xiSetParamString(
-    //         **cam,
-    //         xiapi::XI_PRM_FFC_FLAT_FIELD_FILE_NAME.as_ptr() as *const i8,
-    //         c_void_ptr,
-    //         image.len() as u32,
-    //     );
-
-    //     xiapi::xiSetParamInt(**cam, xiapi::XI_PRM_FFC.as_ptr() as *const i8, 1);
-    // }
+    // Setup AEAG
+    unsafe {
+        xiapi::xiSetParamInt(
+            **cam,
+            xiapi::XI_PRM_AEAG.as_ptr() as *const i8,
+            xiapi::XI_SWITCH::XI_ON.try_into().unwrap(),
+        );
+        xiapi::xiSetParamFloat(**cam, xiapi::XI_PRM_EXP_PRIORITY.as_ptr() as *const i8, 1.0);
+        xiapi::xiSetParamInt(
+            **cam,
+            xiapi::XI_PRM_AE_MAX_LIMIT.as_ptr() as *const i8,
+            2000,
+        );
+        xiapi::xiSetParamFloat(**cam, xiapi::XI_PRM_AEAG_LEVEL.as_ptr() as *const i8, 75.0);
+    }
 
     // recent frame
     cam.recent_frame()?;
@@ -80,6 +61,7 @@ fn get_offset_for_resolution(
     Ok((offset_x, offset_y))
 }
 
+#[allow(dead_code)]
 fn adjust_exposure(exposure: f32, fps: &f32) -> f32 {
     let max_exposure_for_fps = 1_000_000_f32 / fps;
 
@@ -120,4 +102,3 @@ fn set_resolution(
 
     Ok(())
 }
-
